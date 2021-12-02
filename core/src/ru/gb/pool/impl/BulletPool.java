d635215645e4f201362acc4c690c0ff1ff876cb6
Alexey Prikhodko <alvlap@mail.ru>
2021-12-02 16:47:33 +0300

    HomeWork5

--- core/src/ru/gb/pool/impl/BulletPool.java
+++ core/src/ru/gb/pool/impl/BulletPool.java
@@ -1,9 +1,5 @@
 package ru.gb.pool.impl;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 import ru.gb.pool.SpritesPool;
 import ru.gb.sprite.impl.Bullet;
 
